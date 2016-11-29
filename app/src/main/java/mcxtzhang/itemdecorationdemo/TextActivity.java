66a65f2cfec2e2a720be0515f12136577162efe7
Mrz <376238783@qq.com>
2016-11-29 16:08:39 +0800

    commit lushangren

---
+++ app/src/main/java/mcxtzhang/itemdecorationdemo/TextActivity.java
@@ -0,0 +1,15 @@
+package mcxtzhang.itemdecorationdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZYH on 2016/11/29.
+ */
+
+public class TextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
