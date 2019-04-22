14e25fc69959be98dd9523a2e183d68d8e04a879
zhangyaohua <zhangyaohua@ithro.com.cn>
2019-04-22 11:38:42 +0800

    新建分支zyh_law

--- MT/app/src/main/java/com/example/mt/MainActivity.java
+++ MT/app/src/main/java/com/example/mt/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //本地新建的分支zyh_law
     }
 }
